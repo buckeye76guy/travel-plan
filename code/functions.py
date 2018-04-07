9db4052f71078ee48c10f17d3397c1cb08d966f5
buckeye76guy <hounyo.1@osu.edu>
2018-04-07 03:35:38 -0500

    flight info scraped from Expedia

--- code/functions.py
+++ code/functions.py
@@ -1,4 +1,8 @@
 import requests
+import json
+from datetime import datetime as dt
+from dateutil.relativedelta import relativedelta
+from bs4 import BeautifulSoup
 
 
 def get_credentials(path, flavor='api'):
@@ -134,3 +138,49 @@ def getDuration(result):
         return None
 
 # print(getDuration(distanceMatrix(mykey, 'Peoria, IL', 'Chicago, IL', avoid='highways')))
+
+def expedia(origin, destination, start_dt, rtrn=None):
+    url = "https://www.expedia.com/Flights-Search?"
+    rtrn = start_dt + relativedelta(days=2) if rtrn is None else rtrn
+    origin = "from:%s,to:%s,departure:%sTANYT" % (origin, destination, start_dt.strftime("%m/%d/%Y"))
+    destination = "from:%s,to:%s,departure:%sTANYT" % (destination, origin, rtrn.strftime("%m/%d/%Y"))
+    headers = {'mode':'search',
+               'trip':'roundtrip',
+               'leg1':origin,
+               'leg2':destination,
+               'passengers': 'adults:1,children:0,seniors:0,infantinlap:N',
+               'options': 'cabinclass:economy',
+               'origref': 'www.expedia.com'
+               }
+
+    resp = requests.get(url, headers, verify=False)
+    return resp
+
+''' This header definitely worked
+headers = {'mode':'search',
+           'trip':'roundtrip',
+           'leg1':'from:Peoria, IL, United States (PIA),to:Columbus, OH (CMH-John Glenn Columbus Intl.),departure:05/01/2018TANYT',
+           'leg2':'from:Columbus, OH (CMH-John Glenn Columbus Intl.),to:Peoria, IL, United States (PIA),departure:05/01/2018TANYT&',
+           'passengers':'adults:1,children:0,seniors:0,infantinlap:N',
+           'options':'cabinclass:economy',
+           'origref':'www.expedia.com'
+            }
+'''
+
+def getPrice(resp):
+    if resp.status_code != 200:
+        return 'N/A'
+
+    soup = BeautifulSoup(resp.text, "lxml")
+    json_string = soup.find_all(id='cachedResultsJson')
+
+    if len(json_string) == 0:
+        return 'N/A'
+
+    json_string = json_string[0].text.encode('utf-8')
+    json_obj = json.loads(json_string)
+
+    metadata = json_obj.get('metaData',{})
+    return metadata.get('formattedCheapestRoundedUpPrice', 'N/A')
+
+# print(getPrice(expedia('Peoria, IL', 'Columbus, OH', dt(2018,5,12))))
